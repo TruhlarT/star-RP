2a2278e39dfb42c51e14fb0d33c06e67cbddf08a
TruhlarT <32801705+TruhlarT@users.noreply.github.com>
2018-12-05 11:32:40 +0100

    Add files via upload

---
+++ include/star-RPLinkDef.h
@@ -0,0 +1,20 @@
+//#ifdef __CLING__
+#ifdef __CINT__
+
+//#pragma link off all glols;
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class StMuRpsCollection+;
+#pragma link C++ class StMuRpsTrack+;
+#pragma link C++ class StMuRpsTrackPoint+;
+#pragma link C++ class StRpsCluster+;
+#pragma link C++ class StRpsCollection+;
+#pragma link C++ class StRpsPlane+;
+#pragma link C++ class StRpsRomanPot+;
+#pragma link C++ class StRpsTrack+;
+#pragma link C++ class StRpsTrackPoint+;
+
+#endif
+
